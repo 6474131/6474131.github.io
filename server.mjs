c848e042f13a7f9c4a90a5d0f410cee4421863ed
6474131 <lol>
2023-01-15 19:38:47 -0500

    First commit.

---
+++ server.mjs
@@ -0,0 +1,15 @@
+import express, { json as express_json, static as express_static } from "express";
+
+import http from "node:http";
+
+const SERVER_PORT                = 8081;
+
+const app    = express();
+const server = http.createServer({}, app);
+
+app.use(express_json());
+app.use(express_static("dist"));
+
+server.listen(SERVER_PORT, function () {
+  console.log("Listening on port: " + SERVER_PORT);
+});
---
+++ src/index.js
@@ -0,0 +1,146 @@
+import html2canvas from "html2canvas";
+import { BodyTag, CharacterTag } from "./cap-tags";
+import './index.css';
+import './jquery-injectCSS';
+
+require("@eastdesire/jscolor");
+require("webpack-jquery-ui");
+require("webpack-jquery-ui/css");
+
+const DEBUG      = true;
+const reTag      = /\[(.+?)]\s{0,2}("?.+?(?:"|\n|$))/g;
+const reUniQuote = /[\u201C\u201D]/g;
+
+function saveAs(uri, filename) {
+
+  let link = document.createElement('a');
+
+  if (typeof link.download === 'string') {
+
+    link.href     = uri;
+    link.download = filename;
+
+    //Firefox requires the link to be in the body
+    document.body.appendChild(link);
+
+    //simulate click
+    link.click();
+
+    //remove the link when done
+    document.body.removeChild(link);
+
+  } else {
+
+    window.open(uri);
+
+  }
+}
+
+function downloadCap() {
+  let $image       = $("#image");
+  let previousSize = $image.css('width');
+  $image.css('width', $image[0].naturalWidth + "px");
+  html2canvas(
+    document.getElementById("text_holder"),
+    {
+      backgroundColor: $('body').css('background-color'),
+      allowTaint: false,
+      scrollX: 0,
+      scrollY: -window.scrollY,
+    })
+    .then(function (canvas) {
+      saveAs(canvas.toDataURL(), 'cap.png');
+    });
+  $image.css('width', previousSize);
+
+}
+
+/** @type {Object<string, CharacterTag>} */
+let characterTags = {};
+
+/**
+ * For putting new character tags into the dict. Also updates the refill list
+ * with new options.
+ * @param {string} name The name to use for the dictionary.
+ * @param {CharacterTag} tag The character tag to use.
+ */
+function addToCharacterTags(name, tag) {
+  characterTags[name] = tag;
+  let $refill         = $("#current-characters");
+  $refill.empty();
+  for (tag in characterTags) {
+    $refill.append($('<option></option>').val(tag).html(tag));
+  }
+  $refill.val(name);
+}
+
+function convertText(text) {
+  text = text.replace(reUniQuote, '"');
+  text = text.replace(reTag, '<span class="$1">$2</span>');
+  text = text.replaceAll('\n', '<br>');
+  return text;
+}
+
+// async function getFonts() {
+//   const {state} = await navigator.permissions.query({name: 'local-fonts'});
+//   console.log(await queryLocalFonts());
+// }
+
+$(document).ready(() => {
+  $("#raw-text").on('input', () => {
+    let text     = $('#raw-text').val();
+    let new_text = convertText(text);
+    $('#new-text').html(new_text);
+  });
+
+  $("#open-text-options-modal").on('click', () => {
+    $(".popup-overlay, .popup-content").toggleClass('active');
+  });
+  $("#close-text-options-modal").on('click', () => {
+    $(".popup-overlay, .popup-content").removeClass('active');
+  });
+
+  $(".popup-overlay").draggable();
+
+  $("#save-character").on('click', () => {
+    let formJson = JSON.stringify($('#character-form')
+                                    .serializeArray());
+    let tag      = new CharacterTag(formJson);
+    addToCharacterTags(tag['character-name'], tag);
+  });
+  if (!DEBUG) {
+    window.onbeforeunload = function () {
+      return true;
+    };
+  }
+  $("#current-characters").change((el) => {
+    // gets the name of the character selected from the dropdown, then finds
+    // the dictionary entry with that character, and has it refill the form
+    // based on that dictionary entry
+    characterTags[$(el.target).val()].refillForm($("#character-form"));
+  });
+
+  $("#image-upload").on('change', () => {
+    let $demoImage = $("#image");
+    let file       = document.querySelector('input[type=file]').files[0];
+    let reader     = new FileReader();
+    reader.onload  = (event) => {
+      $demoImage[0].src = reader.result;
+    };
+    // I think sometimes it's not a blob if you click on the button and don't
+    // supply a file...? not entirely sure why this happens sometimes
+    if (file instanceof Blob) {
+
+      reader.readAsDataURL(file);
+    }
+  });
+  $('#save-body').on('click', () => {
+    let formJson = JSON.stringify($('#body-form').serializeArray());
+    let tag      = new BodyTag(formJson);
+    //might need to use global variable
+  });
+  $("#download-cap").on('click', () => {
+    downloadCap();
+  });
+
+});
