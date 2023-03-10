fa9de47e1074d28c077f89217f66f7991cbbb3a4
6474131 <lol>
2023-03-09 20:37:59 -0500

    Can now scroll while the offcanvas is open.

--- src/components/sidebar/Offcanvas.vue
+++ src/components/sidebar/Offcanvas.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="offcanvas offcanvas-start" tabindex="-1" :aria-labelledby="offcanvasId + 'label'" :id="offcanvasId">
+  <div class="offcanvas offcanvas-start" tabindex="-1" :aria-labelledby="offcanvasId + 'label'" :id="offcanvasId" data-bs-scroll="true">
     <div class="offcanvas-header">
       <h5 class="offcanvas-title" :id="offcanvasId + 'label'">{{ headerName }}</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
